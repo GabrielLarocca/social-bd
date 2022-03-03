c327964ada9a49d735b964ad1f51a94b9b590fe9
Gabriel Larocca <glarocca@laon.com.br>
2022-03-02 23:15:11 -0300

    migrations ok

---
+++ social-backend/database/migrations/2022_03_03_021336_create_post_fotos_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostFotosTable extends Migration {
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up() {
+		Schema::create('post_fotos', function (Blueprint $table) {
+			$table->id();
+			$table->unsignedBigInteger('pof_id_post');
+			$table->unsignedBigInteger('pof_id_foto');
+			$table->timestamps();
+
+			$table->foreign('pof_id_post')->references('id')->on('posts');
+			$table->foreign('pof_id_foto')->references('id')->on('file');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down() {
+		Schema::dropIfExists('post_fotos');
+	}
+}
